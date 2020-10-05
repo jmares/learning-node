ac8c04e9206b2e1cfc52fbf15109630c7198844c
Johan Mares <67288966+j0hm4r@users.noreply.github.com>
2020-10-05 13:40:34 +0200

    quit exercise because of many typos, lack of explanations, ...

--- tasklist/nodemonapp.js
+++ tasklist/nodemonapp.js
@@ -1,7 +1,8 @@
 var express = require("express");
-var port = 8081;
+var _ = require('lodash');
+var config = require(çonfig)
 var app = express();
-app.listen(port);
+app.listen(config.port);
 
 var taskList = [];
 app.get('/list', function(req, res) {
@@ -11,19 +12,27 @@ app.get('/list', function(req, res) {
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.post('/task', function(req, res) {
+app.post('/task', [validationMiddleware, function(req, res) {
     taskList.push(req.body.task);
     res.send(taskList);
-})
+}])
 
-app.put('/task/:task_index', function(req, res) {
+app.put('/task/:task_index', [validationMiddleware, function(req, res) {
     var taskIndex = req.params.task_index;
     taskList[taskIndex] = req.body.task;
     res.send(taskList);
-});
+}]);
 
 app.delete('/task/:task_index', function(req,res){
     var taskIndex = req.params.task_index;
     taskList.splice(taskIndex, 1);
     res.send(taskList);
 })
+
+function validationMiddleware(req, res, next) {
+    if(_.isEmpty(req.body.task)) {
+        return res.status(422).send("Task is empty");
+    }
+    return next();
+}
+
